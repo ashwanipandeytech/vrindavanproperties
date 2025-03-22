3c7ca0b1379a99533ac2bd3ced7f957a67090111
pushpendra-dwivedi <pushpendradwivedi9@gmail.com>
2025-03-22 19:47:55 +0530

    Add index/about/service pages

---
+++ assets/js/app.js
@@ -0,0 +1,23 @@
+AOS.init();
+
+//COUNTER START HERE
+const counters = document.querySelectorAll('.value');
+const speed = 200;
+
+counters.forEach( counter => {
+   const animate = () => {
+      const value = +counter.getAttribute('akhi');
+      const data = +counter.innerText;
+     
+      const time = value / speed;
+     if(data < value) {
+          counter.innerText = Math.ceil(data + time);
+          setTimeout(animate, 50);
+        }else{
+          counter.innerText = value;
+        }
+     
+   }
+   
+   animate();
+});
