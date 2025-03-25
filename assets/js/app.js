7bcb020886da8fd91c18e8c9c80ecf496f50d885
pushpendra-dwivedi <pushpendradwivedi9@gmail.com>
2025-03-25 22:58:40 +0530

    Done

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,3 +21,16 @@ counters.forEach( counter => {
    
    animate();
 });
+
+let scrollpos = window.scrollY
+const header = document.querySelector("nav")
+const header_height = header.offsetHeight
+
+const add_class_on_scroll = () => header.classList.add("navbar-sticky")
+const remove_class_on_scroll = () => header.classList.remove("navbar-sticky")
+
+window.addEventListener('scroll', function() { 
+  scrollpos = window.scrollY
+  if (scrollpos >= header_height) { add_class_on_scroll() }
+  else { remove_class_on_scroll() }
+})
